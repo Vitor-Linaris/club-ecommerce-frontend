d971107c2a75f05b71ab9762479ffb25a4330b79
Vitor-Linaris <linaris@DESKTOP-D7AD2JN>
2024-07-31 22:56:08 -0300

    feat: add Login Page email and password labels

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -30,9 +30,12 @@ const LoginPage = () => {
           <LoginSubtitle>ou entre com seu e-mail</LoginSubtitle>
 
           <LoginInputContainer>
+            <p>E-mail</p>
             <CustomInput placeholder="Digite seu e-mail" />
           </LoginInputContainer>
+
           <LoginInputContainer>
+            <p>Senha</p>
             <CustomInput placeholder="Digite sua senha"></CustomInput>
           </LoginInputContainer>
 
