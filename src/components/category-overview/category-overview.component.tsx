9b5859c31e55ec923513c42d4d118ff2ddddfd7b
Vitor-Linaris <vitor.linaris@gmail.com>
2024-08-26 10:50:07 -0300

    feat: add Category Overview Component

---
+++ src/components/category-overview/category-overview.component.tsx
@@ -0,0 +1,34 @@
+import { FunctionComponent } from 'react'
+
+// Styles
+import {
+  CategoryContainer,
+  CategoryTitle,
+  ProductsContainer
+} from './category-overview.styles'
+
+// Utilities
+import Category from '../../types/category.types'
+
+// Components
+interface CategoryOverviewProps {
+  category: Category
+}
+
+const CategoryOverview: FunctionComponent<CategoryOverviewProps> = ({
+  category
+}) => {
+  return (
+    <CategoryContainer>
+      <CategoryTitle>{category.displayName}</CategoryTitle>
+
+      <ProductsContainer>
+        {/*  {category.products.slice(0, 4).map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))} */}
+      </ProductsContainer>
+    </CategoryContainer>
+  )
+}
+
+export default CategoryOverview
