def7290edbfcf55a4446144dba51aac387ea8969
Vitor-Linaris <vitor.linaris@gmail.com>
2024-09-24 10:33:14 -0300

    test: ensure custom input renders without error if hasError is false

--- src/components/custom-input/custom-input.spec.tsx
+++ src/components/custom-input/custom-input.spec.tsx
@@ -5,11 +5,21 @@ import Colors from '../../theme/theme.colors'
 describe('Custom Input', () => {
   it('should render with error if hasError is true', () => {
     const { getByPlaceholderText } = render(
-      <CustomInput placeholder="lorem ipsum" hasError={true}></CustomInput>
+      <CustomInput placeholder="lorem ipsum" hasError={true} />
     )
 
     const input = getByPlaceholderText('lorem ipsum')
 
     expect(input).toHaveStyle({ border: `2px solid ${Colors.error}` })
+  })
+
+  it('should render without error if  hasError is false', () => {
+    const { getByPlaceholderText } = render(
+      <CustomInput placeholder="lorem ipsum" hasError={false} />
+    )
+
+    const input = getByPlaceholderText('lorem ipsum')
+
+    expect(input).toHaveStyle({ border: 'none' })
   })
 })
