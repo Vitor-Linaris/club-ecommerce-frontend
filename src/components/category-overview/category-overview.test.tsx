34496b3d8e0b40f92e707fe9414ab510dee0bfee
Vitor-Linaris <vitor.linaris@gmail.com>
2024-10-03 09:33:28 -0300

    test: ensure categories overview fetches and shows categories

--- src/components/category-overview/category-overview.test.tsx
+++ src/components/category-overview/category-overview.test.tsx
@@ -1,6 +1,6 @@
 import { renderWithRedux } from '../../helpers/test.helpers'
 import Category from '../../types/category.types'
-import CategoryOverview from '../category-overview/category-overview.component'
+import CategoryOverview from './category-overview.component'
 
 describe('Category Overview', () => {
   it('should show correct category and its products', () => {
